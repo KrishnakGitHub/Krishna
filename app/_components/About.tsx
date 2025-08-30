e371cf29b1d74d82b68838384e2be86e2a911020
Krishna Kumar <krishnakjee2016@gmail.com>
2025-08-31 00:05:40 +0530

    new update.

---
+++ app/_components/About.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className='w-full px-[10%]'>
+        <div className='grid grid-cols-2 place-items-center min-h-[100vh]'>
+            <div>
+                <h3 className='text-4xl'>👋</h3>
+                <h1 className='text-primary'>Hi! I'm Krishna</h1>
+                <h3 className='text-secondry'>AI Research Engineer | Full Stack Developer</h3>
+            </div>
+            <div className='grid'>
+                  <Image
+                  className="bg-transparent"
+                  src="./loading.svg"
+                  alt="icon"
+                  width={200}
+                  height={100}
+                  priority
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
