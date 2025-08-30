726c5a9fdd469900dd498b4516036351f4de8e45
Krishna Kumar <krishnakjee2016@gmail.com>
2025-08-31 00:10:51 +0530

    fix

--- app/_components/About.tsx
+++ app/_components/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <div className='grid grid-cols-2 place-items-center min-h-[100vh]'>
             <div>
                 <h3 className='text-4xl'>👋</h3>
-                <h1 className='text-primary'>Hi! I'm Krishna</h1>
+                <h1 className='text-primary'>Hi! I&#39;m Krishna</h1>
                 <h3 className='text-secondry'>AI Research Engineer | Full Stack Developer</h3>
             </div>
             <div className='grid'>
