8e3430bfd9489636c78d03a0cb41be3dd21647f9
Krishna Kumar <krishnakjee2016@gmail.com>
2025-09-12 08:29:35 +0530

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Navbar from "./_components/navbar";
 import About from "./_components/About";
 import Experience from "./_components/Experience";
+import FooterSection from "./_components/FooterSection";
+import Contact from "./_components/Contact";
 
 export default function Home() {
   return (
     <div>
-      < Navbar />
-      < About />
-      < Experience />
+      <Navbar />
+      <About />
+      <Experience />
+      <Contact />
+      <FooterSection />
     </div>
   );
 }
